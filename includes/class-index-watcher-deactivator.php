f34e262492dda5e1c372a45b29eb5c0a73752c28
webkonditorei <59343694+webkonditorei@users.noreply.github.com>
2024-09-09 09:58:10 +0200

    Update class-index-watcher-deactivator.php

--- includes/class-index-watcher-deactivator.php
+++ includes/class-index-watcher-deactivator.php
@@ -12,7 +12,7 @@ class Index_Watcher_Deactivator
 
 	{
 
-		$timestamp = wp_next_scheduled('my_custom_cronjob');
-		wp_unschedule_event($timestamp, 'my_custom_cronjob');
+		$timestamp = wp_next_scheduled('index_watcher_cronjob');
+		wp_unschedule_event($timestamp, 'index_watcher_cronjob');
 	}
 }
